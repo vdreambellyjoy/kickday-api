13f05d98c8edb35793987becd436eaeabb54e219
prem_kumar_pentapati <p.premkumar@julius66.com>
2023-10-02 22:11:36 +0530

    maker list

--- Controller/authController.js
+++ Controller/authController.js
@@ -27,6 +27,61 @@ const authController = {
             res.send({ success: false, code: 500, data: err, message: 'something went wrong' })
         }
     },
+    saveProfile: async (req, res) => {
+        try {
+            let update = await req.mongoConnection.collection('users').updateOne(
+                {email: req.body.email},
+                { $set : {profile: req.body, isMaker: true}},
+                {upsert: true}
+            )
+            if (update) {
+                res.status(200).send({ success: true, code: 200, data: update, message: 'success' })
+            } else {
+                res.status(400).send({ success: false, code: 400, data: update, message: 'something went wrong' })
+            }
+        } catch(err) {
+            console.log(err, 8787);
+            // await logErrors({mongoConnection: req.mongoConnection, err});
+            res.send({ success: false, code: 500, data: err, message: 'something went wrong' })
+        }
+    },
+    saveBankDetails: async (req, res) => {
+        try {
+            let update = await req.mongoConnection.collection('users').updateOne(
+                {email: req.body.email},
+                { $set : {bankDetails: req.body, draftDone: true}},
+                {upsert: true}
+            )
+            if (update) {
+                res.status(200).send({ success: true, code: 200, data: update, message: 'success' })
+            } else {
+                res.status(400).send({ success: false, code: 400, data: update, message: 'something went wrong' })
+            }
+        } catch(err) {
+            console.log(err, 8787);
+            // await logErrors({mongoConnection: req.mongoConnection, err});
+            res.send({ success: false, code: 500, data: err, message: 'something went wrong' })
+        }
+    },
+    getMakers: async (req, res) => {
+        try {
+            let match = {email: {$ne: 'premkumar.231996@gmail.com'}}
+            if (req.body.match == 'all') {
+
+            } else if (req.body.match == 'isMaker') {
+                match = { email: {$ne: 'premkumar.231996@gmail.com'}, draftDone: true}
+            } else if (req.body.match == 'draftDone') {
+                match = { email: {$ne: 'premkumar.231996@gmail.com'}, draftDone: {$exists: false}}
+            }
+            console.log(req.body);
+            let list = await req.mongoConnection.collection('users').find(match).toArray();
+            res.status(200).send({ success: true, code: 200, data: list, message: 'success' })
+        } catch(err) {
+            console.log(err, 8787);
+            // await logErrors({mongoConnection: req.mongoConnection, err});
+            res.send({ success: false, code: 500, data: err, message: 'something went wrong' })
+        }
+    }
 }
 
 module.exports = authController;
--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -2,6 +2,9 @@ const auth = require('express').Router();
 const authController = require('../Controller/authController');
 
 auth.post('/login', authController.login);
+auth.post('/saveProfile', authController.saveProfile);
+auth.post('/saveBankDetails', authController.saveBankDetails);
+auth.post('/getMakers', authController.getMakers);
 
 
 module.exports = auth;
